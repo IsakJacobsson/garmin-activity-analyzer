205505343628512a8ee59e4070c2dc7c57f3fad7
Isak Jacobsson <isak2jacobsson@gmail.com>
2026-01-25 13:02:20 +0100

    Fix start and end date of plot

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from metrics import aggregate_metric_over_time, get_valid_metrics
 def activity_metrics_over_time_section(df):
     st.header("Activity metrics over time")
 
+    start_date = df["Datum"].min()
+    end_date = df["Datum"].max()
+
     col1, col2 = st.columns(2)
 
     with col1:
@@ -29,10 +32,18 @@ def activity_metrics_over_time_section(df):
     tab_day, tab_week, tab_month, tab_year = st.tabs(["Day", "Week", "Month", "Year"])
 
     # Plot each tab
-    plot_metric_tab(activity_df, selected_metric, "D", "%Y-%m-%d", tab_day)  # Day
-    plot_metric_tab(activity_df, selected_metric, "W", "%Y-%W", tab_week)  # Week
-    plot_metric_tab(activity_df, selected_metric, "M", "%Y-%m", tab_month)  # Month
-    plot_metric_tab(activity_df, selected_metric, "Y", "%Y", tab_year)  # Year
+    plot_metric_tab(
+        activity_df, selected_metric, "D", "%Y-%m-%d", tab_day, start_date, end_date
+    )  # Day
+    plot_metric_tab(
+        activity_df, selected_metric, "W", "%Y-%W", tab_week, start_date, end_date
+    )  # Week
+    plot_metric_tab(
+        activity_df, selected_metric, "M", "%Y-%m", tab_month, start_date, end_date
+    )  # Month
+    plot_metric_tab(
+        activity_df, selected_metric, "Y", "%Y", tab_year, start_date, end_date
+    )  # Year
 
 
 def activity_multiselector(df):
@@ -63,8 +74,8 @@ def convert_time_column_to_hours(df):
     return df
 
 
-def plot_metric_tab(df, metric, freq, fmt, tab):
-    agg_df = aggregate_metric_over_time(df, metric, freq)
+def plot_metric_tab(df, metric, freq, fmt, tab, start_date, end_date):
+    agg_df = aggregate_metric_over_time(df, metric, freq, start_date, end_date)
 
     plot_df = agg_df.copy()
     plot_df.index = plot_df.index.to_timestamp(how="start")
--- metrics.py
+++ metrics.py
@@ -23,19 +23,12 @@ def get_valid_metrics(df):
     return valid_metrics
 
 
-def aggregate_metric_over_time(df, metric, period_freq, end_period=None):
+def aggregate_metric_over_time(df, metric, period_freq, start, end):
     df = df.copy()
     df["Period"] = df["Datum"].dt.to_period(period_freq)
 
     agg = df.groupby("Period")[metric].sum().sort_index()
 
-    if end_period is None:
-        end_period = pd.Timestamp.today().to_period(period_freq)
-
-    full_range = pd.period_range(
-        start=agg.index.min(),
-        end=end_period,
-        freq=period_freq,
-    )
+    full_range = pd.period_range(start=start, end=end, freq=period_freq)
 
     return agg.reindex(full_range, fill_value=0).to_frame(metric)
